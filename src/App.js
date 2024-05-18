f800fd1cb85ecd49dd6e1dd8bbae909ea2270c7e
aAoo-dev <axxrxxo@gmail.com>
2024-05-18 14:37:14 +0200

    added click button to show email in alert

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
 
-        let email = 'fracz@agh.edu.pl';
+
+    let message = <h1> Podaj adres</h1>;
+
+    const [email, setEmail] = useState('artur@agh.edu.pl');
 
 
-        function handleChange(event) {
+    function handleChange(event) {
 
-                console.log(event.target.value);
+        setEmail(event.target.value);
+    }
 
-        }
+    if (message.length > 0) {
+        message = <h2>masz krótki adres </h2>
+    }
+
+    if (email.length > 10) {
+
+        message = <div>troche dłuższy adres!</div>;
+
+    } else if (email.length > 15) {
+
+        message = <div>Twój adres e-mail jest w sam raz.</div>;
+
+    } else {
+
+        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
 
 
-        return (
+    }
 
-            <div>
 
-                    <h1>System do zapisów na zajęcia</h1>
+    return (
 
-                    <h2>Twój e-mail to {email}.</h2>
+        <div>
 
-                    <input type="text" onChange={handleChange}/>
+            <h1>System do zapisów na zajęcia</h1>
 
-            </div>
+            <h2>Twój e-mail to {email}.</h2>
+            {message}
 
-        );
+            <input type="text" onChange={handleChange}/>
+            <button type="button" onClick={() => alert(email)}>
+
+                pokaz adres email w alercie.
+
+            </button>
+
+        </div>
+
+    );
+
 
 }
 
